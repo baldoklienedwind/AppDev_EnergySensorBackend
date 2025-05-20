5cf0fbeae1a3f5c1998fdfdc592997627c5634d3
baldoklienedwind <baldoklienedwind@gmail.com>
2025-05-20 06:33:02 -0400

    Update main.py

--- energy-meter-backend/app/main.py
+++ energy-meter-backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://your-frontend-deployment.com", 
+    "https://appdev-energysensorwebfrontend.onrender.com", 
     "*",
 ]
 
