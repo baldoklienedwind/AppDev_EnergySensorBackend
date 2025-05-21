55b388fa9d4970ea359c0a882204fd3aec681bc8
baldoklienedwind <baldoklienedwind@gmail.com>
2025-05-21 06:07:19 -0400

    db_renewal

--- energy-meter-backend/app/database.py
+++ energy-meter-backend/app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://energy_meter_db_ag2t_user:FTPqi2yChZrdvcaxsXqOy9siodJg0I0Z@dpg-d0m3lip5pdvs738si0sg-a/energy_meter_db_ag2t"
+    "postgresql+asyncpg://energy_sensor_db_user:ju2BOAjiUkXgy0whqjyA9XHANszk1sX6@dpg-d0mq8k63jp1c738j4740-a/energy_sensor_db"
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
