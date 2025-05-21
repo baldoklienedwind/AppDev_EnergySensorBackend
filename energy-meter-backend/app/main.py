9392c5c77b9e936eb4c2ba720fd71480295f6a13
baldoklienedwind <baldoklienedwind@gmail.com>
2025-05-21 00:56:11 -0400

    Update main.py

--- energy-meter-backend/app/main.py
+++ energy-meter-backend/app/main.py
@@ -12,12 +12,12 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://appdev-energysensorwebfrontend.onrender.com",
-    "exp://",
+    "exp://", 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
